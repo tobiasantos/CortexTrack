a7a3d34614f709c4990a1e29d70235ce0e10eac5
Tobias Santos <tobiassantosn@gmail.com>
2026-03-02 00:02:40 -0300

    extensao prod

--- extension/src/utils/constants.js
+++ extension/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:3000/api";
+export const API_BASE_URL = "https://cortextrack-production.up.railway.app/api";
 
 export const IDLE_THRESHOLD_SECONDS = 60;
 
